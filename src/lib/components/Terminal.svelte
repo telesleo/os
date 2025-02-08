3251685eb105b04d6c085712dc6cfadfbdafbe34
Leonardo Andrei <leonardoandrei41@gmail.com>
2025-02-07 23:24:38 -0300

    Implement an input field to the terminal component

--- src/lib/components/Terminal.svelte
+++ src/lib/components/Terminal.svelte
@@ -1,1 +1,15 @@
-<h1>terminal</h1>
+<script lang="ts">
+  let command = $state("");
+
+  function execute() {
+    console.log(command);
+  }
+
+  function onkeydown(event: KeyboardEvent) {
+    if (event.key === "Enter") {
+      execute();
+    }
+  }
+</script>
+
+<input type="text" {onkeydown} bind:value={command} />
