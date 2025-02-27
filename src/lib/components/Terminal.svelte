2c531845521f65be213d9e5d3deb125d7dc3ac26
Leonardo Andrei <leonardoandrei41@gmail.com>
2025-02-26 21:29:05 -0300

    Change the style of terminal

--- src/lib/components/Terminal.svelte
+++ src/lib/components/Terminal.svelte
@@ -48,24 +48,36 @@
 
 <div class="terminal">
 	{#if history.length > 0}
-		<ul class="terminal-history">
+		<ul class="history">
 			{#each history as historyPiece}
 				<li>{historyPiece}</li>
 			{/each}
 		</ul>
 	{/if}
-	<input type="text" {onkeydown} bind:value={command} />
+	<input class="input" type="text" {onkeydown} bind:value={command} />
 </div>
 
 <style>
 	.terminal {
-		background-color: black;
+		background-color: var(--black);
 		width: 100%;
 		height: 100%;
 		padding: 15px;
+		box-sizing: border-box;
 	}
 
-	.terminal-history {
-		color: white;
+	.history {
+		color: var(--white);
+		list-style-type: none;
+		padding: 0;
+		margin: 0;
+	}
+
+	.input {
+		background-color: var(--black);
+		color: var(--white);
+		outline: 0;
+		border: 0;
+		width: 100%;
 	}
 </style>
