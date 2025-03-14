28341ed0e608aa0e1384fe8cc5f22a6f5211dddd
Leonardo Andrei <leonardoandrei41@gmail.com>
2025-03-13 21:10:35 -0300

    Fix cursor type style

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,43 @@
   import "./variables.css";
 
   import Main from "./components/Main.svelte";
-  // import time from "./lib/data/time.svelte";
+  import { onMount } from "svelte";
+  import type { Coordinate } from "./lib/base/general";
 
-  // $inspect(time.value);
+  const mouseButton: boolean[] = $state([false, false, false]);
+  const mousePosition: Coordinate = $state({ x: -1, y: -1 });
+  let cursorType: string = $state("default");
+
+  function setCursorType(newCursorType: string) {
+    cursorType = newCursorType;
+  }
+
+  function mouseMove(event: MouseEvent) {
+    mousePosition.x = event.clientX;
+    mousePosition.y = event.clientY;
+  }
+
+  function mouseDown(event: MouseEvent) {
+    mouseButton[event.button] = true;
+  }
+
+  function mouseUp(event: MouseEvent) {
+    mouseButton[event.button] = false;
+  }
+
+  onMount(() => {
+    window.addEventListener("mousemove", mouseMove);
+    window.addEventListener("mousedown", mouseDown);
+    window.addEventListener("mouseup", mouseUp);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+      window.removeEventListener("mousedown", mouseDown);
+      window.removeEventListener("mouseup", mouseUp);
+    };
+  });
 </script>
 
-<main>
-  <Main></Main>
+<main style={`cursor: ${cursorType}`}>
+  <Main {mouseButton} {mousePosition} {setCursorType}></Main>
 </main>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,41 +1,8 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import type { Coordinate } from "../lib/base/general";
   import Bar from "./Bar.svelte";
   import Boxes from "./Boxes.svelte";
 
-  const mouseButton: boolean[] = $state([false, false, false]);
-  const mousePosition: Coordinate = $state({ x: -1, y: -1 });
-  let cursorType: string = $state("default");
-
-  function setCursorType(newCursorType: string) {
-    cursorType = newCursorType;
-  }
-
-  function mouseMove(event: MouseEvent) {
-    mousePosition.x = event.clientX;
-    mousePosition.y = event.clientY;
-  }
-
-  function mouseDown(event: MouseEvent) {
-    mouseButton[event.button] = true;
-  }
-
-  function mouseUp(event: MouseEvent) {
-    mouseButton[event.button] = false;
-  }
-
-  onMount(() => {
-    window.addEventListener("mousemove", mouseMove);
-    window.addEventListener("mousedown", mouseDown);
-    window.addEventListener("mouseup", mouseUp);
-
-    return () => {
-      window.removeEventListener("mousemove", mouseMove);
-      window.removeEventListener("mousedown", mouseDown);
-      window.removeEventListener("mouseup", mouseUp);
-    };
-  });
+  const { mouseButton, mousePosition, setCursorType } = $props();
 </script>
 
 <div id="screen"></div>
